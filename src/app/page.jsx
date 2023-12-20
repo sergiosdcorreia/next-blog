80db5fb58de8b1c1a7aaae770592a708637168d4
Sérgio Correia <sergiosdcorreia@gmail.com>
2023-12-20 16:19:46 +0000

    Adding structural components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,18 @@
+import CardList from "@/components/cardList/CardList";
+import CategoryList from "@/components/categoryList/CategoryList";
+import Featured from "@/components/featured/Featured";
+import Menu from "@/components/menu/Menu";
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.container}>
+        <CardList />
+        <Menu />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/cardList/CardList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Pagination from "../pagination/Pagination";
+import styles from "./cardList.module.css"
+
+const CardList = () => {
+    return (
+        <div className={styles.container}>CardList
+        <Pagination />
+        </div>
+    )
+}
+
+export default CardList
