2252147f7a371b309c297e06de5e54c36dde6601
Sérgio Correia <sergiosdcorreia@gmail.com>
2023-12-21 04:35:40 +0000

    Adding auth links and theme toggle components

---
+++ src/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./authLinks.module.css"
+
+const AuthLinks = () => {
+    return (
+        <div className={styles.container}>AuthLinks</div>
+    )
+}
+
+export default AuthLinks
