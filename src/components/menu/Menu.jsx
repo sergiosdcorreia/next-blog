c23239db452909889563a1d4da6e6cea43ecf3eb
Sérgio Correia <sergiosdcorreia@gmail.com>
2023-12-31 11:52:06 +0000

    Splitting Menu Categories code to its own component

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./menu.module.css"
 import Link from "next/link";
-import Image from "next/image";
 import MenuPosts from "../menuPosts/MenuPosts";
+import MenuCategories from "../menuCategories/MenuCategories";
 
 const Menu = () => {
     return (
@@ -12,12 +12,7 @@ const Menu = () => {
             <MenuPosts withImage={false} />
             <h2 className={styles.subtitle}>Discover by topic</h2>
             <h1 className={styles.title}>Categories</h1>
-            <div className={styles.categoryList}>
-                <Link href="/blog?cat=style" className={`${styles.categoryItem} ${styles.style}`}>Style</Link>
-                <Link href="/blog" className={`${styles.categoryItem} ${styles.fashion}`}>Fashion</Link>
-                <Link href="/blog" className={`${styles.categoryItem} ${styles.food}`}>Food</Link>
-                <Link href="/blog" className={`${styles.categoryItem} ${styles.travel}`}>Travel</Link>
-            </div>
+            <MenuCategories />
             <h2 className={styles.subtitle}>Chosen by the editor</h2>
             <h1 className={styles.title}>Editor picks</h1>
             <MenuPosts withImage={true} />
---
+++ src/components/menuCategories/MenuCategories.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./menuCategories.module.css"
+import Link from "next/link";
+
+const MenuCategories = () => {
+    return (
+        <div className={styles.categoryList}>
+            <Link href="/blog?cat=style" className={`${styles.categoryItem} ${styles.style}`}>Style</Link>
+            <Link href="/blog" className={`${styles.categoryItem} ${styles.fashion}`}>Fashion</Link>
+            <Link href="/blog" className={`${styles.categoryItem} ${styles.food}`}>Food</Link>
+            <Link href="/blog" className={`${styles.categoryItem} ${styles.travel}`}>Travel</Link>
+        </div>
+    )
+}
+
+export default MenuCategories
