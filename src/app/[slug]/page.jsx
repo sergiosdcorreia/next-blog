1fda9f1ba415c1408bf13952c4cfd1cc12a323ef
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-01-01 13:17:50 +0000

    Adding single page blog

---
+++ src/app/[slug]/page.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+import styles from './singlePage.module.css'
+import Menu from "@/components/menu/Menu"
+import Image from "next/image"
+
+const SinglePage = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.infoContainer}>
+                <div className={styles.textContainer}>
+                    <h1>Lorem ipsum dolor sit amet...</h1>
+                    <div className={styles.user}>
+                        <div className={styles.userImageContainer}>
+                            <Image src="/p1.jpeg" alt="" fill className={styles.avatar} />
+                        </div>
+                        <div className={styles.userTextContainer}>
+                            <span className={styles.username}>John Doe</span>
+                            <span className={styles.date}>01.01.2024</span>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.imageContainer}>
+                    <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+                </div>
+            </div>
+            <div className={styles.content}>
+                <div className={styles.post}>
+                    <div className={styles.description}>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <Menu />
+            </div>
+        </div>
+    )
+}
+
+export default SinglePage
