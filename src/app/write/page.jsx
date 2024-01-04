6fe40423ff8318f84883913c7a7c9c00123097c5
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-01-04 23:57:51 +0000

    Adding auth logic to the Write Page

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -5,10 +5,23 @@ import styles from './writePage.module.css'
 import Image from 'next/image'
 import ReactQuill from 'react-quill'
 import "react-quill/dist/quill.bubble.css"
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 
 const WritePage = () => {
+
+    const router = useRouter()
+    
+    const { status } = useSession()
     const [open, setOpen] = useState(false)
     const [value, setValue] = useState('')
+    
+    if (status === "loading") {
+        return <div className={styles.loading}>Loading...</div>
+    }
+    if (status === "authenticated") {
+        router.push("/")
+    }
 
     return (
         <div className={styles.container}>
