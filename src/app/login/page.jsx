af20b736a59765b7e7e4afc3b552719b7e32b3de
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-01-04 11:48:50 +0000

    Login and logout with google

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,12 +3,20 @@
 import React from 'react'
 import styles from './loginPage.module.css'
 import { signIn, useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 
 const LoginPage = () => {
 
     const {data, status} = useSession()
 
-    console.log(data, status)
+    const router = useRouter()
+
+    if (status === "loading") {
+        return <div className={styles.loading}>Loading...</div>
+    }
+    if (status === "authenticated") {
+        router.push("/")
+    }
     return (
         <div className={styles.container}>
             <div className={styles.wrapper}>
--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -3,6 +3,7 @@
 import Link from "next/link"
 import React, { useState } from "react"
 import styles from "./authLinks.module.css"
+import { signOut } from "next-auth/react"
 
 const AuthLinks = () => {
 
@@ -14,7 +15,7 @@ const AuthLinks = () => {
         ) : (
             <>
                 <Link href="/write" className={styles.link}>Write</Link>
-                <span className={styles.link}>Logout</span>
+                <span className={styles.link} onClick={signOut}>Logout</span>
             </>
         )}
         <div className={styles.burger} onClick={() => setOpen(!open)}>
